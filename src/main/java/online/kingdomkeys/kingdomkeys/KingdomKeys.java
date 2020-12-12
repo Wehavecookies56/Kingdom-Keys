72fcb25a315a7820b594af6db9ad9c9fc4ddebb7
Stel312 <nstelken312@gmail.com>
2020-12-12 12:59:26 -0600

    oogle projector

--- src/main/java/online/kingdomkeys/kingdomkeys/KingdomKeys.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/KingdomKeys.java
@@ -78,7 +78,7 @@ public class KingdomKeys {
 	public static final String MCVER = "1.15.2";
 
 	// The proxy instance created for the current dist double lambda prevents class being loaded on the other dist
-	public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ProxyClient(), () -> () -> new ProxyServer());
+	public static IProxy proxy = DistExecutor.safeRunForDist(() -> ProxyClient::new, () -> ProxyServer::new);
 
 	public static ItemGroup orgWeaponsGroup = new ItemGroup(Strings.organizationGroup) {
 		@Override
