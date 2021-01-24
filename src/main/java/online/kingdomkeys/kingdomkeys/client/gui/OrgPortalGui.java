f4c1019a35973fbda8611ff91e5e77835f7c5a3f
Abelatox <abel54225@gmail.com>
2021-01-24 23:18:50 +0100

    Update OrgPortalGui.java

--- src/main/java/online/kingdomkeys/kingdomkeys/client/gui/OrgPortalGui.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/gui/OrgPortalGui.java
@@ -1,10 +1,16 @@
 package online.kingdomkeys.kingdomkeys.client.gui;
 
+import java.util.Map;
+import java.util.UUID;
+
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
+import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
+import online.kingdomkeys.kingdomkeys.entity.block.OrgPortalTileEntity;
+import online.kingdomkeys.kingdomkeys.lib.PortalData;
 import online.kingdomkeys.kingdomkeys.network.PacketHandler;
 import online.kingdomkeys.kingdomkeys.network.cts.CSSetOrgPortalName;
 
@@ -12,7 +18,7 @@ public class OrgPortalGui extends Screen {
 
 	TextFieldWidget nameBox;
 	Button set;
-	
+
 	BlockPos pos;
 
 	public OrgPortalGui(BlockPos pos) {
@@ -24,9 +30,18 @@ public class OrgPortalGui extends Screen {
 	protected void init() {
 		int tfWidth = minecraft.fontRenderer.getStringWidth("####################");
 		addButton(nameBox = new TextFieldWidget(minecraft.fontRenderer, width / 2 - tfWidth / 2, height / 2 - 10, tfWidth, 16, ("")));
-		addButton(set = new Button(width / 2 - tfWidth / 2, height / 2 +10, tfWidth, 20, "Set name", (e) -> {
+		addButton(set = new Button(width / 2 - tfWidth / 2, height / 2 + 10, tfWidth, 20, "Set name", (e) -> {
 			action();
 		}));
+
+		if (minecraft.player.world.getTileEntity(pos) != null && minecraft.player.world.getTileEntity(pos) instanceof OrgPortalTileEntity) {
+			OrgPortalTileEntity te = (OrgPortalTileEntity) minecraft.player.world.getTileEntity(pos);
+			UUID portalUUID = te.getUUID();
+			Map<UUID, PortalData> portals = ModCapabilities.getWorld(minecraft.player.world).getPortals();
+			String text = ModCapabilities.getWorld(minecraft.player.world).getPortalFromUUID(portalUUID).getName();
+
+			nameBox.setText(text);
+		}
 		super.init();
 	}
 
