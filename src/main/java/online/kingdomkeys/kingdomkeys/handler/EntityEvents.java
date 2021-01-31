89065fd0246ccf905cf778c4e2e17adbf14611ec
Abelatox <abel54225@gmail.com>
2021-01-31 14:49:30 +0100

    Fixed crash when manually reloading arrowguns when the ammo is 0

--- src/main/java/online/kingdomkeys/kingdomkeys/handler/EntityEvents.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/handler/EntityEvents.java
@@ -851,6 +851,8 @@ public class EntityEvents {
 		nPlayer.setHealth(oldPlayerData.getMaxHP());
 		nPlayer.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(oldPlayerData.getMaxHP());
 		
+		PacketHandler.sendTo(new SCSyncWorldCapability(ModCapabilities.getWorld(nPlayer.world)), (ServerPlayerEntity)nPlayer);
+
 	}
 	
 	@SubscribeEvent
