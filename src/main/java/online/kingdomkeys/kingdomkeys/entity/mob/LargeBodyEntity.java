761ce20aeef304cbfdea545eef997c66183e2cc7
Abelatox <abel54225@gmail.com>
2021-02-01 01:07:16 +0100

    Large body bounce

--- src/main/java/online/kingdomkeys/kingdomkeys/entity/mob/LargeBodyEntity.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/entity/mob/LargeBodyEntity.java
@@ -26,7 +26,7 @@ import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.entity.EntityHelper;
 import online.kingdomkeys.kingdomkeys.entity.ModEntities;
 
-public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, IKHMob {
+public class LargeBodyEntity extends MonsterEntity implements IKHMob {
 
     enum SpecialAttack {
         WAIT,
@@ -35,8 +35,6 @@ public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, 
         SHOCKWAVE;
     }
 
-    public PartEntity[] partsArray;
-    public PartEntity partBelly = new PartEntity(ModEntities.TYPE_LARGE_BODY.get(), this, "strongzoneBelly");
     private SpecialAttack currentAttack, previousAttack;
     private int timeForNextAI = 80;
     private boolean isAngry = false;
@@ -53,7 +51,6 @@ public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, 
 
     public LargeBodyEntity(FMLPlayMessages.SpawnEntity spawnEntity, World world) {
         super(ModEntities.TYPE_LARGE_BODY.get(), world);
-        this.partsArray = new PartEntity[] { partBelly };
     }
     
     @Override
@@ -93,9 +90,6 @@ public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, 
     public void tick() {
         super.tick();
 
-        this.partBelly.size = new EntitySize(2.2F, 2.4F, false);
-        this.partBelly.recalculateSize();
-
         int rotation = MathHelper.floor(this.getRotationYawHead() * 4.0F / 360.0F + 0.5D) & 3;
 
         if(this.getHealth() < this.getMaxHealth()/3)
@@ -128,28 +122,29 @@ public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, 
             //KingdomKeys.proxy.spawnDarkSmokeParticle(world, getPosX(), getPosY() + 1, getPosZ(), 0, 0.01D, 0, 0.8F);
         }
 
-        if(EntityHelper.getState(this) != 10 || EntityHelper.getState(this) != 2)
-            newBellyPos(rotation);
     }
 
-    private void newBellyPos(int rotation) {
-        switch(rotation) {
-            case 0:
-                this.partBelly.setPosition(this.getPosX(), this.getPosY(), this.getPosZ() + 0.8);
-                break;
-            case 1:
-                this.partBelly.setPosition(this.getPosX() - 0.8, this.getPosY(), this.getPosZ());
-                break;
-            case 2:
-                this.partBelly.setPosition(this.getPosX(), this.getPosY(), this.getPosZ() - 0.8);
-                break;
-            case 3:
-                this.partBelly.setPosition(this.getPosX() + 0.8, this.getPosY(), this.getPosZ());
-                break;
-        }
-        this.partBelly.tick();
+    @Override
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+    	if(source.getTrueSource() instanceof LivingEntity) {
+    		Entity attacker = source.getImmediateSource();
+    		double d1 = attacker.getPosX() - this.getPosX();
+            double d0 = attacker.getPosZ() - this.getPosZ();
+            float attackYaw = (float)Math.toDegrees((MathHelper.atan2(d0, d1)));// Global degree the attack is coming from
+            float diff = MathHelper.wrapDegrees(attackYaw-rotationYaw);
+
+    		if(diff > 30 && diff < 150) {
+    			if(attacker instanceof LivingEntity) {
+	                ((LivingEntity) attacker).applyKnockback(0.8F, -d1, -d0);
+	                attacker.setMotion(attacker.getMotion().x, 0.5F, attacker.getMotion().z);
+    			}
+                return false;    		
+    		}
+    	}
+    	return super.attackEntityFrom(source, amount);
     }
 
+    
     public boolean attackEntityAsMob(Entity ent) {
         int i = 0;
         float j = 1;
@@ -165,21 +160,8 @@ public class LargeBodyEntity extends MonsterEntity implements IMultiPartEntity, 
         return ent.attackEntityFrom(DamageSource.causeMobDamage(this), i * j);
     }
 
-    public boolean attackEntityFromPart(PartEntity part, DamageSource source, float damage) {
-        if(part.getPartName().contains("strongzone")) {
-            return false;
-        }
-
-        this.attackEntityFrom(source, damage);
-        return true;
-    }
-
     public World getWorld() {
         return this.world;
-    }
-
-    public Entity[] getParts() {
-        return this.partsArray;
     }
 
     public void setCurrentAttackState(SpecialAttack state) {
