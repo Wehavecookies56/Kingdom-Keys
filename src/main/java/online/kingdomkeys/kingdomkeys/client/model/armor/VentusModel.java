a3c43de7f50a43a3206c9a544c64556b888a5cb0
Wehavecookies56 <qwenit56@gmail.com>
2022-03-25 11:16:58 +0000

    Actually make use of size in VentusModel

--- src/main/java/online/kingdomkeys/kingdomkeys/client/model/armor/VentusModel.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/model/armor/VentusModel.java
@@ -67,45 +67,45 @@ public class VentusModel<T extends LivingEntity> extends HumanoidModel<T> {
         MeshDefinition meshdefinition = HumanoidModel.createMesh(size, 0);
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition horn1 = partdefinition.addOrReplaceChild("horn1", CubeListBuilder.create().texOffs(0, 32).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-3.5F, -12.0F, 0.0F, -0.2737F, 0.0F, 0.0F));
+        PartDefinition horn1 = partdefinition.addOrReplaceChild("horn1", CubeListBuilder.create().texOffs(0, 32).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 4.0F, 2.0F, size), PartPose.offsetAndRotation(-3.5F, -12.0F, 0.0F, -0.2737F, 0.0F, 0.0F));
 
-        PartDefinition horn2 = partdefinition.addOrReplaceChild("horn2", CubeListBuilder.create().texOffs(0, 32).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.5F, -12.0F, 0.0F, -0.2737F, 0.0F, 0.0F));
+        PartDefinition horn2 = partdefinition.addOrReplaceChild("horn2", CubeListBuilder.create().texOffs(0, 32).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 4.0F, 2.0F, size), PartPose.offsetAndRotation(4.5F, -12.0F, 0.0F, -0.2737F, 0.0F, 0.0F));
 
-        PartDefinition rightElbowBase = partdefinition.addOrReplaceChild("rightElbowBase", CubeListBuilder.create().texOffs(36, 32).addBox(-3.5F, -2.0F, -3.5F, 7.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(6.0F, 7.0F, 0.0F, 0.0F, -3.1416F, 0.0F));
+        PartDefinition rightElbowBase = partdefinition.addOrReplaceChild("rightElbowBase", CubeListBuilder.create().texOffs(36, 32).addBox(-3.5F, -2.0F, -3.5F, 7.0F, 5.0F, 7.0F, size), PartPose.offsetAndRotation(6.0F, 7.0F, 0.0F, 0.0F, -3.1416F, 0.0F));
 
-        PartDefinition rightElbowSpike = rightElbowBase.addOrReplaceChild("rightElbowSpike", CubeListBuilder.create().texOffs(26, 38).addBox(-0.5F, -3.0F, -1.5F, 1.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.6F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3491F));
+        PartDefinition rightElbowSpike = rightElbowBase.addOrReplaceChild("rightElbowSpike", CubeListBuilder.create().texOffs(26, 38).addBox(-0.5F, -3.0F, -1.5F, 1.0F, 3.0F, 3.0F, size), PartPose.offsetAndRotation(-2.6F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3491F));
 
-        PartDefinition rightElbowSpike2 = rightElbowSpike.addOrReplaceChild("rightElbowSpike2", CubeListBuilder.create().texOffs(29, 44).addBox(-0.5F, -3.0F, -1.0F, 1.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.1F, -2.9F, 0.0F, 0.0F, 0.0F, 0.126F));
+        PartDefinition rightElbowSpike2 = rightElbowSpike.addOrReplaceChild("rightElbowSpike2", CubeListBuilder.create().texOffs(29, 44).addBox(-0.5F, -3.0F, -1.0F, 1.0F, 3.0F, 2.0F, size), PartPose.offsetAndRotation(-0.1F, -2.9F, 0.0F, 0.0F, 0.0F, 0.126F));
 
-        PartDefinition rightKneeBase = partdefinition.addOrReplaceChild("rightKneeBase", CubeListBuilder.create().texOffs(16, 32).addBox(-2.4F, 0.0F, -0.5F, 4.0F, 1.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(1.9F, 16.0F, -1.9F));
+        PartDefinition rightKneeBase = partdefinition.addOrReplaceChild("rightKneeBase", CubeListBuilder.create().texOffs(16, 32).addBox(-2.4F, 0.0F, -0.5F, 4.0F, 1.0F, 5.0F, size), PartPose.offset(1.9F, 16.0F, -1.9F));
 
-        PartDefinition rightKnee1 = rightKneeBase.addOrReplaceChild("rightKnee1", CubeListBuilder.create().texOffs(31, 32).addBox(-1.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition rightKnee1 = rightKneeBase.addOrReplaceChild("rightKnee1", CubeListBuilder.create().texOffs(31, 32).addBox(-1.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, size), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.7854F));
 
-        PartDefinition rightKneeSpike = rightKneeBase.addOrReplaceChild("rightKneeSpike", CubeListBuilder.create().texOffs(17, 32).addBox(-0.5F, -2.8F, -0.4F, 0.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.4F, 0.0F, 2.0F, 0.0F, 0.0F, 0.3128F));
+        PartDefinition rightKneeSpike = rightKneeBase.addOrReplaceChild("rightKneeSpike", CubeListBuilder.create().texOffs(17, 32).addBox(-0.5F, -2.8F, -0.4F, 0.0F, 2.0F, 0.0F, size), PartPose.offsetAndRotation(2.4F, 0.0F, 2.0F, 0.0F, 0.0F, 0.3128F));
 
-        PartDefinition rightKnee2 = rightKneeBase.addOrReplaceChild("rightKnee2", CubeListBuilder.create().texOffs(31, 34).addBox(-0.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition rightKnee2 = rightKneeBase.addOrReplaceChild("rightKnee2", CubeListBuilder.create().texOffs(31, 34).addBox(-0.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, size), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
-        PartDefinition rightFoot = partdefinition.addOrReplaceChild("rightFoot", CubeListBuilder.create().texOffs(0, 41).addBox(-1.9F, 0.0F, -3.0F, 4.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(1.9F, 21.0F, -0.9F));
+        PartDefinition rightFoot = partdefinition.addOrReplaceChild("rightFoot", CubeListBuilder.create().texOffs(0, 41).addBox(-1.9F, 0.0F, -3.0F, 4.0F, 3.0F, 2.0F, size), PartPose.offset(1.9F, 21.0F, -0.9F));
 
-        PartDefinition rightFootTip = rightFoot.addOrReplaceChild("rightFootTip", CubeListBuilder.create().texOffs(16, 41).addBox(-1.9F, 0.0F, -1.0F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -3.0F));
+        PartDefinition rightFootTip = rightFoot.addOrReplaceChild("rightFootTip", CubeListBuilder.create().texOffs(16, 41).addBox(-1.9F, 0.0F, -1.0F, 4.0F, 2.0F, 1.0F, size), PartPose.offset(0.0F, 1.0F, -3.0F));
 
-        PartDefinition leftElbowBase = partdefinition.addOrReplaceChild("leftElbowBase", CubeListBuilder.create().texOffs(36, 32).addBox(-3.5F, -2.0F, -3.5F, 7.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, 7.0F, 0.0F));
+        PartDefinition leftElbowBase = partdefinition.addOrReplaceChild("leftElbowBase", CubeListBuilder.create().texOffs(36, 32).addBox(-3.5F, -2.0F, -3.5F, 7.0F, 5.0F, 7.0F, size), PartPose.offset(-6.0F, 7.0F, 0.0F));
 
-        PartDefinition leftElbowSpike = leftElbowBase.addOrReplaceChild("leftElbowSpike", CubeListBuilder.create().texOffs(26, 38).addBox(-0.5F, -3.0F, -1.5F, 1.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.6F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3491F));
+        PartDefinition leftElbowSpike = leftElbowBase.addOrReplaceChild("leftElbowSpike", CubeListBuilder.create().texOffs(26, 38).addBox(-0.5F, -3.0F, -1.5F, 1.0F, 3.0F, 3.0F, size), PartPose.offsetAndRotation(-2.6F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3491F));
 
-        PartDefinition leftElbowSpike2 = leftElbowSpike.addOrReplaceChild("leftElbowSpike2", CubeListBuilder.create().texOffs(29, 44).addBox(-0.5F, -3.0F, -1.0F, 1.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.1F, -2.9F, 0.0F, 0.0F, 0.0F, 0.126F));
+        PartDefinition leftElbowSpike2 = leftElbowSpike.addOrReplaceChild("leftElbowSpike2", CubeListBuilder.create().texOffs(29, 44).addBox(-0.5F, -3.0F, -1.0F, 1.0F, 3.0F, 2.0F, size), PartPose.offsetAndRotation(-0.1F, -2.9F, 0.0F, 0.0F, 0.0F, 0.126F));
 
-        PartDefinition leftKneeBase = partdefinition.addOrReplaceChild("leftKneeBase", CubeListBuilder.create().texOffs(16, 32).addBox(-2.5F, 0.0F, -0.5F, 4.0F, 1.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.9F, 16.0F, -1.9F));
+        PartDefinition leftKneeBase = partdefinition.addOrReplaceChild("leftKneeBase", CubeListBuilder.create().texOffs(16, 32).addBox(-2.5F, 0.0F, -0.5F, 4.0F, 1.0F, 5.0F, size), PartPose.offset(-1.9F, 16.0F, -1.9F));
 
-        PartDefinition leftKnee1 = leftKneeBase.addOrReplaceChild("leftKnee1", CubeListBuilder.create().texOffs(31, 32).addBox(-1.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.7854F));
+        PartDefinition leftKnee1 = leftKneeBase.addOrReplaceChild("leftKnee1", CubeListBuilder.create().texOffs(31, 32).addBox(-1.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, size), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.7854F));
 
-        PartDefinition leftKneeSpike = leftKneeBase.addOrReplaceChild("leftKneeSpike", CubeListBuilder.create().texOffs(17, 32).addBox(-0.49F, -2.8F, -0.4F, 0.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 0.0F, 2.0F, 0.0F, 0.0F, -0.3128F));
+        PartDefinition leftKneeSpike = leftKneeBase.addOrReplaceChild("leftKneeSpike", CubeListBuilder.create().texOffs(17, 32).addBox(-0.49F, -2.8F, -0.4F, 0.0F, 2.0F, 0.0F, size), PartPose.offsetAndRotation(-2.0F, 0.0F, 2.0F, 0.0F, 0.0F, -0.3128F));
 
-        PartDefinition leftKnee2 = leftKneeBase.addOrReplaceChild("leftKnee2", CubeListBuilder.create().texOffs(31, 34).addBox(-0.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition leftKnee2 = leftKneeBase.addOrReplaceChild("leftKnee2", CubeListBuilder.create().texOffs(31, 34).addBox(-0.2F, 0.5F, -0.5F, 1.0F, 1.0F, 0.0F, size), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
-        PartDefinition leftFoot = partdefinition.addOrReplaceChild("leftFoot", CubeListBuilder.create().texOffs(0, 41).addBox(-2.1F, 0.0F, -3.0F, 4.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.9F, 21.0F, -0.9F));
+        PartDefinition leftFoot = partdefinition.addOrReplaceChild("leftFoot", CubeListBuilder.create().texOffs(0, 41).addBox(-2.1F, 0.0F, -3.0F, 4.0F, 3.0F, 2.0F, size), PartPose.offset(-1.9F, 21.0F, -0.9F));
 
-        PartDefinition leftFootTip = leftFoot.addOrReplaceChild("leftFootTip", CubeListBuilder.create().texOffs(16, 41).addBox(-2.1F, 0.0F, -1.0F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -3.0F));
+        PartDefinition leftFootTip = leftFoot.addOrReplaceChild("leftFootTip", CubeListBuilder.create().texOffs(16, 41).addBox(-2.1F, 0.0F, -1.0F, 4.0F, 2.0F, 1.0F, size), PartPose.offset(0.0F, 1.0F, -3.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
