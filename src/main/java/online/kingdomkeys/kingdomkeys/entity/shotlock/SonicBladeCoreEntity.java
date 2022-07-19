1f5fd5e3a20a86faae947e3e28ae6c3a3d67c5a9
Abelatox <abel54225@gmail.com>
2022-07-19 17:55:08 +0200

    Update SonicBladeCoreEntity.java

--- src/main/java/online/kingdomkeys/kingdomkeys/entity/shotlock/SonicBladeCoreEntity.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/entity/shotlock/SonicBladeCoreEntity.java
@@ -58,63 +58,25 @@ public class SonicBladeCoreEntity extends ThrowableProjectile{
 		setTarget(targetIDS.substring(1));
 		this.targetList = targets;
 		this.dmg = dmg;
-        player.startAutoSpinAttack(20 * targets.size());
-
 	}
 
 	@Override
 	public void tick() {
+		getCaster().startAutoSpinAttack(10);
+
 		if(tickCount % 8 == 1) {
-			//System.out.println(getTarget());
 			if (getCaster() != null && getTarget() != null) {
 				BlockPos pos = getTarget().blockPosition();
-				float speedFactor = 0.5F;
+				float speedFactor = 0.4F;
 				getCaster().setDeltaMovement((pos.getX() - getCaster().getX()) * speedFactor, (pos.getY() - getCaster().getY()) * speedFactor, (pos.getZ() - getCaster().getZ()) * speedFactor);
 	
 				if (level.isClientSide) {
 					getCaster().hurtMarked = true;
 				}
-					
-				double dx = getCaster().getX() - getTarget().getX();
-	            double dz = getCaster().getZ() - getTarget().getZ();
-	            double dy = getCaster().getY() - (getTarget().getY() + (getTarget().getBbHeight() / 2.0F)-getCaster().getBbHeight());
-	            double yaw = Math.atan2(dz, dx) * 180 / Math.PI;
-	            double pitch = Math.atan2(dy, Math.sqrt(dx * dx + dz * dz)) * 180 / Math.PI;
-	            double distance = getCaster().distanceTo(getTarget());
-	            
-	            float rYaw = (float) Mth.wrapDegrees(yaw - getCaster().getXRot()) + 90;
-	            float rPitch = (float) pitch - (float) (10.0F / Math.sqrt(distance)) + (float) (distance * Math.PI / 90);
-	            /*
-	            float f = getCaster().getXRot();
-	            float f1 = getCaster().getYRot();
-	            
-	            getCaster().setYRot((float)((double)getCaster().getYRot() + (double)rYaw * 0.15D));
-	            getCaster().setXRot((float)((double)getCaster().getXRot() - (double)-(rPitch - getCaster().getXRot()) * 0.15D));
-	            getCaster().xRotO = getCaster().getXRot() - f;
-	            getCaster().yRotO += getCaster().getYRot() - f1;*/
-	            Player player = getCaster();
-	            
-                /*float f7 = rPitch;
-                float f = rYaw;
-                float f1 = -Mth.sin(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
-                float f2 = -Mth.sin(f * ((float)Math.PI / 180F));
-                float f3 = Mth.cos(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
-                float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
-                float f5 = 3.0F * ((1.0F + (float)1) / 4.0F);
-                f1 *= f5 / f4;
-                f2 *= f5 / f4;
-                f3 *= f5 / f4;
-                player.push((double)f1, (double)f2, (double)f3);*/
-                /*if (player.isOnGround()) { //Leap
-                   float f6 = 1.1999999F;
-                   player.move(MoverType.SELF, new Vec3(0.0D, f6, 0.0D));
-                }*/
 	
 	            if (getCaster().getVehicle() != null) {
 	            	getCaster().getVehicle().onPassengerTurned(getCaster());
 	            }
-	            	            
-	           // updateMovement();
 	            setActualTargetIndex(getActualTargetIndex()+1);
 			} else {
 				kill();
@@ -147,43 +109,6 @@ public class SonicBladeCoreEntity extends ThrowableProjectile{
 		super.tick();
 	}
 
-	private void updateMovement() {
-		if(getTarget() != null) {
-			if(getTarget().isAlive()) {
-				this.shoot(getTarget().getX() - this.getX(), (getTarget().getY() + (getTarget().getBbHeight() / 2.0F) - this.getBbHeight()) - getY() + 0.5, getTarget().getZ() - this.getZ(), 1, 0);
-			} else {
-				if(getOwner() != null)
-					this.shootFromRotation(this, getOwner().getXRot(), getOwner().getYRot(), 0, 1, 0); // Work in progress
-			}
-			
-			//Damage
-		}
-	}
-
-	/*@Override
-	protected void onHit(HitResult rtRes) {
-		if (!level.isClientSide) {
-			EntityHitResult ertResult = null;
-			BlockHitResult brtResult = null;
-
-			if (rtRes instanceof EntityHitResult) {
-				ertResult = (EntityHitResult) rtRes;
-			}
-
-			if (rtRes instanceof BlockHitResult) {
-				brtResult = (BlockHitResult) rtRes;
-			}
-
-			if (ertResult != null && ertResult.getEntity() instanceof LivingEntity) {
-				LivingEntity target = (LivingEntity) ertResult.getEntity();
-				if (target != getOwner()) {
-					target.hurt(DamageSource.thrown(this, this.getOwner()), dmg);
-					super.remove(RemovalReason.KILLED);
-				}
-			}
-			remove(RemovalReason.KILLED);
-		}
-	}*/
 	
 	public int getMaxTicks() {
 		return maxTicks;
