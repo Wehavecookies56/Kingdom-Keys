1f90f6655ff7de92f2c526f6c0a559dbe085a392
Wehavecookies56 <qwenit56@gmail.com>
2022-08-06 16:59:57 +0100

    Fix Incomplete X-blade description

--- src/main/java/online/kingdomkeys/kingdomkeys/datagen/provider/KeybladeProvider.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/datagen/provider/KeybladeProvider.java
@@ -1,9 +1,12 @@
 package online.kingdomkeys.kingdomkeys.datagen.provider;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -12,16 +15,18 @@ import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.google.gson.JsonElement;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import online.kingdomkeys.kingdomkeys.datagen.builder.KeybladeBuilder;
+import org.apache.commons.lang3.text.translate.JavaUnicodeEscaper;
 
 public abstract class KeybladeProvider<T extends KeybladeBuilder<T>> implements DataProvider {
 
-    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
+    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().disableHtmlEscaping().create();
     protected final DataGenerator generator;
     protected final String modid;
     protected final Function<ResourceLocation, T> factory;
@@ -62,11 +67,42 @@ public abstract class KeybladeProvider<T extends KeybladeBuilder<T>> implements 
         for (T model : generatedModels.values()) {
             Path target = getPath(model);
             try {
-                DataProvider.save(GSON, cache, model.toJson(), target);
+                save(GSON, cache, model.toJson(), target);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    static void save(Gson pGson, HashCache pCache, JsonElement pJsonElement, Path pPath) throws IOException {
+        String s = pGson.toJson(pJsonElement);
+        s = JavaUnicodeEscaper.outsideOf(0, 0x7f).translate(s);
+        String s1 = SHA1.hashUnencodedChars(s).toString();
+        if (!Objects.equals(pCache.getHash(pPath), s1) || !Files.exists(pPath)) {
+            Files.createDirectories(pPath.getParent());
+            BufferedWriter bufferedwriter = Files.newBufferedWriter(pPath);
+
+            try {
+                bufferedwriter.write(s);
+            } catch (Throwable throwable1) {
+                if (bufferedwriter != null) {
+                    try {
+                        bufferedwriter.close();
+                    } catch (Throwable throwable) {
+                        throwable1.addSuppressed(throwable);
+                    }
+                }
+
+                throw throwable1;
+            }
+
+            if (bufferedwriter != null) {
+                bufferedwriter.close();
+            }
+        }
+
+        pCache.putNew(pPath, s1);
     }
 
     private Path getPath(T model) {
