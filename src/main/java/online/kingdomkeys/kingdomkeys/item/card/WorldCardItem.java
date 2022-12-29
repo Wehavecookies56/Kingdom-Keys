b7ae76b9840c2f1edd9b4782c8d9e019146f253c
Abelatox <abel54225@gmail.com>
2022-12-29 18:43:48 +0100

    Tried to make world cards change floor biome
    Still needs to clear the floor in order to generate it properly

--- src/main/java/online/kingdomkeys/kingdomkeys/item/card/WorldCardItem.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/item/card/WorldCardItem.java
@@ -1,8 +1,22 @@
 package online.kingdomkeys.kingdomkeys.item.card;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import online.kingdomkeys.kingdomkeys.KingdomKeys;
+import online.kingdomkeys.kingdomkeys.capability.CastleOblivionCapabilities;
+import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.Floor;
 import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.FloorType;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.ModRoomTypes;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.Room;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.RoomData;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.RoomGenerator;
+import online.kingdomkeys.kingdomkeys.world.dimension.castle_oblivion.system.RoomUtils;
 
 import java.util.function.Supplier;
 
@@ -17,5 +31,28 @@ public class WorldCardItem extends Item {
 
     public FloorType getFloorType() {
         return floorType.get();
+    }
+    
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+    	if(!pLevel.isClientSide) {
+	        CastleOblivionCapabilities.ICastleOblivionInteriorCapability cap = ModCapabilities.getCastleOblivionInterior(pLevel);
+	        if (cap != null) {
+	            Room currentRoom = cap.getRoomAtPos(pPlayer.blockPosition());
+	            if (currentRoom == null) {
+	                KingdomKeys.LOGGER.info("something is wrong player should be in the lobby room");
+	            } else {
+	                Floor floor = cap.getFloors().get(0);
+	                if (floor != null) {
+	                	//Clear the whole floor (typical mom quote)
+	                	//Clear the whole path
+	                	floor.setWorldCard(this);
+                        RoomData data = floor.getRoom(new RoomUtils.RoomPos(0, 1));
+                        Room newRoom = RoomGenerator.INSTANCE.generateRoom(data, ModRoomTypes.SLEEPING_DARKNESS.get(), pPlayer, currentRoom, RoomUtils.Direction.NORTH, false);
+	                }
+	            }
+	        }
+    	}
+    	return super.use(pLevel, pPlayer, pUsedHand);
     }
 }
