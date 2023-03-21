532b1c554b4795370bc9196f66395fda23fa3904
Abelatox <abel54225@gmail.com>
2023-03-22 00:33:39 +0100

    forgot

--- src/main/java/online/kingdomkeys/kingdomkeys/magic/MagicDataLoader.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/magic/MagicDataLoader.java
@@ -55,6 +55,9 @@ public class MagicDataLoader extends SimpleJsonResourceReloadListener {
     public void loadData(ResourceManager manager) {
         String folder = "magics";
         String extension = ".json";
+        
+        names.clear();
+        dataList.clear();
 
         for (ResourceLocation file : manager.listResources(folder, n -> n.toString().endsWith(extension)).keySet()) { //Get all .json files
             ResourceLocation magicName = new ResourceLocation(file.getNamespace(), file.getPath().substring(folder.length() + 1, file.getPath().length() - extension.length()));
