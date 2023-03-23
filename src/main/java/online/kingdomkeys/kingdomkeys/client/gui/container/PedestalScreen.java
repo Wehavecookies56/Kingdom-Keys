e539edcf8fc6892597f2ca28d3e309d7747bfe4e
Abelatox <abel54225@gmail.com>
2023-03-23 22:58:55 +0100

    Upgraded forge and fixed pedestal and kkchest models

--- src/main/java/online/kingdomkeys/kingdomkeys/client/gui/container/PedestalScreen.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/gui/container/PedestalScreen.java
@@ -5,7 +5,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
