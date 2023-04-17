5d47d982db9e55172a926eb81c21c8de9c5cd051
Stel312 <nstelken312@gmail.com>
2023-04-16 21:24:17 -0500

    keyblade shoots only once now

--- src/main/java/online/kingdomkeys/kingdomkeys/integration/epicfight/init/KKAnimations.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/integration/epicfight/init/KKAnimations.java
@@ -1,14 +1,6 @@
 package online.kingdomkeys.kingdomkeys.integration.epicfight.init;
 
-import com.mojang.math.Matrix4f;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -16,25 +8,18 @@ import online.kingdomkeys.kingdomkeys.KingdomKeys;
 import online.kingdomkeys.kingdomkeys.client.sound.ModSounds;
 import online.kingdomkeys.kingdomkeys.entity.organization.ArrowgunShotEntity;
 import online.kingdomkeys.kingdomkeys.lib.DamageCalculation;
-import yesman.epicfight.api.animation.Animator;
-import yesman.epicfight.api.animation.ServerAnimator;
 import yesman.epicfight.api.animation.property.AnimationEvent;
 import yesman.epicfight.api.animation.property.AnimationProperty;
-import yesman.epicfight.api.animation.types.AttackAnimation;
 import yesman.epicfight.api.animation.types.BasicAttackAnimation;
 import yesman.epicfight.api.animation.types.StaticAnimation;
-import yesman.epicfight.api.client.animation.ClientAnimator;
 import yesman.epicfight.api.forgeevent.AnimationRegistryEvent;
 import yesman.epicfight.api.utils.math.OpenMatrix4f;
 import yesman.epicfight.api.utils.math.Vec3f;
 import yesman.epicfight.gameasset.Armatures;
-import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
-
-import java.awt.*;
 
 public class KKAnimations {
     public static StaticAnimation TEST, CHAKRAM_AUTO_1, ROXAS_AUTO_1, ROXAS_IDLE, ROXAS_RUN,
-            KK_SHIELD_AUTO_1, KK_SHIELD_AUTO_2, KK_SHIELD_AUTO_3, KH1_SORA_AUTO_1, VALOR_IDLE, VALOR_AUTO_1, VALOR_AUTO_2,
+            KK_SHIELD_AUTO_1, KK_SHIELD_AUTO_2, KK_SHIELD_AUTO_3, KH1_SORA_COMBO1, VALOR_IDLE, VALOR_AUTO_1, VALOR_AUTO_2,
             VALOR_AUTO_3, MASTER_IDLE, WISDOM_IDLE, WISDOM_RUN, WISDOM_COMBO1;
 
 
@@ -53,7 +38,7 @@ public class KKAnimations {
         WISDOM_IDLE = new StaticAnimation(true, "biped/living/wisdom_idle", Armatures.BIPED);
         WISDOM_RUN = new StaticAnimation(true, "biped/living/wisdom_run", Armatures.BIPED);
         WISDOM_COMBO1= new BasicAttackAnimation(0.16F, 0.05F, 0.5F, 0.7F, KKColiders.KEYBLADE,Armatures.BIPED.toolR,"biped/combat/kh1_sora_auto_1",  Armatures.BIPED)
-                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, 1.1F).addEvents(AnimationProperty.StaticAnimationProperty.EVENTS, AnimationEvent.TimeStampedEvent
+                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED, 1.1F).addEvents(AnimationEvent.TimeStampedEvent
                         .create(.2f, (ep, arr) -> {
 
                             Player player = (Player) ep.getOriginal();
@@ -66,7 +51,6 @@ public class KKAnimations {
                                         .mulBack(OpenMatrix4f.createRotatorDeg(180, Vec3f.Y_AXIS)).mulBack(ep.getModelMatrix(1));
                                 Vec3 test2 = OpenMatrix4f.transform(Armatures.BIPED.getBindedTransformFor(ep.getArmature().getPose(1)
                                         , Armatures.BIPED.toolR).mulFront(tempMatrix) , Vec3.ZERO);
-
 
                                 ArrowgunShotEntity shot = new ArrowgunShotEntity(player.level, player, DamageCalculation.getMagicDamage(player) * 0.1F,test2.x, test2.y, test2.z );
                                 shot.setShotType(1);
@@ -85,7 +69,7 @@ public class KKAnimations {
         ROXAS_IDLE = new StaticAnimation(true,"biped/living/roxas_idle",  Armatures.BIPED);
         ROXAS_RUN = new StaticAnimation(true,"biped/living/roxas_run",  Armatures.BIPED);
 
-        KH1_SORA_AUTO_1 = new BasicAttackAnimation(0.16F, 0.05F, 0.5F, 0.7F, KKColiders.KEYBLADE,Armatures.BIPED.toolR,"biped/combat/kh1_sora_auto_1",  Armatures.BIPED);
+        KH1_SORA_COMBO1 = new BasicAttackAnimation(0.16F, 0.05F, 0.5F, 0.7F, KKColiders.KEYBLADE,Armatures.BIPED.toolR,"biped/combat/kh1_sora_auto_1",  Armatures.BIPED);
 
         KK_SHIELD_AUTO_1 = new BasicAttackAnimation(0.16F, 0.05F, 0.16F, 0.7F, null, Armatures.BIPED.toolR,"biped/combat/kk_shield_auto_1",  Armatures.BIPED);
         KK_SHIELD_AUTO_2 = new BasicAttackAnimation(0.16F, 0.05F, 0.16F, 0.7F, null,Armatures.BIPED.toolR,"biped/combat/kk_shield_auto_2",  Armatures.BIPED);
