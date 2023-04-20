6f1ceba7b857baf489bef55d124b9550ad35b79f
Abelatox <abel54225@gmail.com>
2023-04-20 17:51:17 +0200

    Fixed keyblade damage output from equipment gui

--- src/main/java/online/kingdomkeys/kingdomkeys/client/gui/elements/buttons/MenuSelectEquipmentButton.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/gui/elements/buttons/MenuSelectEquipmentButton.java
@@ -187,7 +187,7 @@ public class MenuSelectEquipmentButton extends MenuButtonBase {
 					String strengthStr = String.valueOf(((int) keyblade.getStrength(stack)));
 					String magicStr = String.valueOf(((int) keyblade.getMagic(stack)));
 					IPlayerCapabilities playerData = ModCapabilities.getPlayer(minecraft.player);
-					int strength = playerData.getStrength(true) + ((int) keyblade.getStrength(stack));
+					int strength = playerData.getStrength(true) + ((int) keyblade.getStrength(stack)); //TODO a
 					int magic = playerData.getMagic(true) + ((int) keyblade.getMagic(stack));
 					
 					String totalStrengthStr = String.valueOf(strength);
