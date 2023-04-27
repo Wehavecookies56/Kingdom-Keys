3dd644c6b9467eb452594bf72944563fe1ba9b69
Abelatox <abel54225@gmail.com>
2023-04-27 19:42:17 +0200

    Version Bump

--- src/main/java/online/kingdomkeys/kingdomkeys/KingdomKeys.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/KingdomKeys.java
@@ -77,7 +77,7 @@ public class KingdomKeys {
 
 	public static final String MODID = "kingdomkeys";
 	public static final String MODNAME = "Kingdom Keys";
-	public static final String MODVER = "2.2.1.0";
+	public static final String MODVER = "2.2.1.1";
 	public static final String MCVER = "1.19.2";
 
 	public static CreativeModeTab orgWeaponsGroup = new CreativeModeTab(Strings.organizationGroup) {
