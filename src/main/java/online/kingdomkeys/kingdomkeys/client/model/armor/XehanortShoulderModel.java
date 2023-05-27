35c94cc29e9f2d03fc65350feccb25f645daf162
Abelatox <abel54225@gmail.com>
2023-05-27 23:03:33 +0200

    Eraqus and Xehanort shoulder models

--- src/main/java/online/kingdomkeys/kingdomkeys/client/model/armor/XehanortShoulderModel.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/model/armor/XehanortShoulderModel.java
@@ -33,16 +33,41 @@ public class XehanortShoulderModel<T extends LivingEntity> extends HumanoidModel
         
         PartDefinition leftArm = partdefinition.getChild("left_arm");
         
-		PartDefinition ButtonArmor = leftArm.addOrReplaceChild("ButtonArmor", CubeListBuilder.create().texOffs(1, 30).addBox(7.0F, -20.6F, -1.5F, 0.65F, 3.2F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, 20.9F, 0.0F));
+		PartDefinition ShoulderArmor = leftArm.addOrReplaceChild("ShoulderArmor", CubeListBuilder.create().texOffs(21, 61).addBox(2.1F, -2.3F, -0.2F, 0.25F, 2.3F, 0.4F, new CubeDeformation(0.0F))
+		.texOffs(21, 61).addBox(2.1F, 0.4F, -0.2F, 0.25F, 1.6F, 0.4F, new CubeDeformation(0.0F))
+		.texOffs(38, 56).addBox(2.1F, -1.7064F, -2.1308F, 0.15F, 3.5F, 4.3F, new CubeDeformation(0.0F)), PartPose.offset(0.1F, 0.7F, 0.0F));
 
-		PartDefinition Armor3_r1 = ButtonArmor.addOrReplaceChild("Armor3_r1", CubeListBuilder.create().texOffs(0, 9).addBox(-0.125F, -1.0F, -2.0F, 0.25F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(7.375F, -14.8F, 0.0F, 0.0F, 0.0F, -0.2618F));
+		PartDefinition cube_r1 = ShoulderArmor.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(28, 58).addBox(-1.9F, -2.0F, -1.0F, 0.15F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, 2.5F, 0.0F, 0.7854F, 0.0F, 0.0F));
 
-		PartDefinition Armor2_r1 = ButtonArmor.addOrReplaceChild("Armor2_r1", CubeListBuilder.create().texOffs(0, 17).addBox(-0.125F, -1.0F, -2.0F, 0.25F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(7.375F, -15.9F, 0.0F, 0.0F, 0.0F, -0.2618F));
+		PartDefinition cube_r2 = ShoulderArmor.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(28, 58).addBox(-1.9F, -2.0F, -1.0F, 0.15F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(4.0F, -1.0F, 0.0F, 0.7854F, 0.0F, 0.0F));
 
-		PartDefinition Armor1_r1 = ButtonArmor.addOrReplaceChild("Armor1_r1", CubeListBuilder.create().texOffs(0, 9).addBox(-0.125F, -1.0F, -2.0F, 0.25F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(7.375F, -17.2F, 0.0F, 0.0F, 0.0F, -0.2618F));
+		PartDefinition cube_r3 = ShoulderArmor.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(20, 60).addBox(-0.125F, -0.4F, -0.4F, 0.25F, 0.8F, 0.8F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, -2.5F, 0.0F, 0.7854F, 0.0F, 0.0F));
 
-		PartDefinition Button_r1 = ButtonArmor.addOrReplaceChild("Button_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, -1.0F, -1.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(7.5F, -19.0F, 0.0F, -0.7854F, 0.0F, 0.0F));
-		
+		PartDefinition cube_r4 = ShoulderArmor.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.4F, -0.4F, 0.25F, 0.7F, 0.7F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, -1.3F, 1.1F, 0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r5 = ShoulderArmor.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.4F, -0.3F, 0.25F, 0.7F, 0.7F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, -1.3F, -1.1F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r6 = ShoulderArmor.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.5F, -0.5F, 0.25F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 1.6F, -0.2F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r7 = ShoulderArmor.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.5F, -0.5F, 0.25F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 1.6F, 0.2F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r8 = ShoulderArmor.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.5F, -0.5F, 0.25F, 1.6F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 1.6F, 1.0F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r9 = ShoulderArmor.addOrReplaceChild("cube_r9", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.2F, -0.8F, 0.25F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(21, 61).addBox(-0.125F, -0.3F, -0.9F, 0.25F, 1.0F, 1.2F, new CubeDeformation(0.0F))
+		.texOffs(21, 61).addBox(-0.125F, -0.4F, -1.0F, 0.25F, 1.0F, 1.4F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 1.6F, 1.0F, 0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r10 = ShoulderArmor.addOrReplaceChild("cube_r10", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.2F, -0.2F, 0.25F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(21, 61).addBox(-0.125F, -0.3F, -0.3F, 0.25F, 1.0F, 1.2F, new CubeDeformation(0.0F))
+		.texOffs(21, 61).addBox(-0.125F, -0.4F, -0.4F, 0.25F, 1.0F, 1.4F, new CubeDeformation(0.0F))
+		.texOffs(20, 60).addBox(-0.125F, -0.5F, -0.5F, 0.25F, 1.0F, 1.6F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 1.6F, -1.0F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r11 = ShoulderArmor.addOrReplaceChild("cube_r11", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.4F, -0.4F, 0.25F, 0.8F, 0.8F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, -1.3F, 0.0F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r12 = ShoulderArmor.addOrReplaceChild("cube_r12", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.5F, -0.5F, 0.25F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, 0.2F, 0.0F, -0.7854F, 0.0F, 0.0F));
+
+		PartDefinition cube_r13 = ShoulderArmor.addOrReplaceChild("cube_r13", CubeListBuilder.create().texOffs(21, 61).addBox(-0.125F, -0.9F, -0.2F, 0.25F, 1.8F, 0.4F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.225F, -1.3F, 0.0F, -1.5708F, 0.0F, 0.0F));
+
 		return LayerDefinition.create(meshdefinition, 64, 64);
     }
 
