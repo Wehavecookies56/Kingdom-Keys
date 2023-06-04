26a9144d828fa3db1d9f3d66ac50dee102441754
Abelatox <abel54225@gmail.com>
2023-06-04 14:51:22 +0200

    Fixed player portrait showing some animations

--- src/main/java/online/kingdomkeys/kingdomkeys/client/render/AeroLayerRenderer.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/render/AeroLayerRenderer.java
@@ -4,12 +4,16 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import com.mojang.math.Vector3f;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -20,24 +24,36 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import online.kingdomkeys.kingdomkeys.capability.IGlobalCapabilities;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
+import online.kingdomkeys.kingdomkeys.util.IDisabledAnimations;
 
 @OnlyIn(Dist.CLIENT)
 public class AeroLayerRenderer<T extends LivingEntity> extends RenderLayer<T, PlayerModel<T>> {
-	   public static final ResourceLocation TEXTURE = new ResourceLocation("textures/entity/trident_riptide.png");
-	   public static final String BOX = "box";
-	   private final ModelPart box;
+	public static final ResourceLocation TEXTURE = new ResourceLocation("textures/entity/trident_riptide.png");
+	public static final String BOX = "box";
+	private final ModelPart box;
 
 	public AeroLayerRenderer(RenderLayerParent<T, PlayerModel<T>> p_174540_, EntityModelSet p_174541_) {
-	      super(p_174540_);
-	      ModelPart modelpart = p_174541_.bakeLayer(ModelLayers.PLAYER_SPIN_ATTACK);
-	      this.box = modelpart.getChild("box");
-	   }
+		super(p_174540_);
+		ModelPart modelpart = p_174541_.bakeLayer(ModelLayers.PLAYER_SPIN_ATTACK);
+		this.box = modelpart.getChild("box");
+	}
 
 	@Override
 	public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-		if(ModCapabilities.getGlobal(entitylivingbaseIn) != null) {
+		if (entitylivingbaseIn instanceof AbstractClientPlayer) {
+			LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderer = (LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>>) Minecraft.getInstance().getEntityRenderDispatcher().getRenderer((AbstractClientPlayer) entitylivingbaseIn);
+			if (!((IDisabledAnimations) renderer).isDisabled()) {
+				renderEntity(matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
+			}
+		} else {
+			renderEntity(matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
+		}
+	}
+
+	public void renderEntity(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+		if (ModCapabilities.getGlobal(entitylivingbaseIn) != null) {
 			IGlobalCapabilities globalData = ModCapabilities.getGlobal(entitylivingbaseIn);
-			if(globalData.getAeroTicks() > 0) {
+			if (globalData.getAeroTicks() > 0) {
 				VertexConsumer vertexconsumer = bufferIn.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
 
 		         for(int i = 1; i <= globalData.getAeroLevel() + 1; ++i) {
@@ -57,34 +73,35 @@ public class AeroLayerRenderer<T extends LivingEntity> extends RenderLayer<T, Pl
 			            	scale = 0.35F*i;
 		            		matrixStackIn.scale(scale,scale,scale);
 
-		            	}
-		            	break;
-		            case 1:
-		            	if(entitylivingbaseIn instanceof Player) {
-			            	scale = 0.85F*i;
-		            		matrixStackIn.scale(scale,scale*1F,scale);
-			            	matrixStackIn.translate(0.0D, (double)(-0.8F + 0.8F * (float)i), 0.0D);
-		            	} else {
-			            	scale = 0.45F*i;
-		            		matrixStackIn.scale(scale,scale,scale);
-		            	}
 
-		            	break;
-		            case 2:
-			            if(entitylivingbaseIn instanceof Player) {
-			            	scale = 0.7F*i;
-				            matrixStackIn.scale(scale,scale*0.6F,scale);
-				            matrixStackIn.translate(0.0D, (double)(-1.2F + 0.6F * (float)i), 0.0D);
-		            	} else {
-			            	scale = 0.55F*i;
-		            		matrixStackIn.scale(scale,scale*0.6F,scale);
-		            	}
-		            	break;
-		            	
-		            }
-		            this.box.render(matrixStackIn, vertexconsumer, packedLightIn, OverlayTexture.NO_OVERLAY);
-		            matrixStackIn.popPose();
-		         }
+						}
+						break;
+					case 1:
+						if (entitylivingbaseIn instanceof Player) {
+							scale = 0.85F * i;
+							matrixStackIn.scale(scale, scale * 1F, scale);
+							matrixStackIn.translate(0.0D, (double) (-0.8F + 0.8F * (float) i), 0.0D);
+						} else {
+							scale = 0.45F * i;
+							matrixStackIn.scale(scale, scale, scale);
+						}
+
+						break;
+					case 2:
+						if (entitylivingbaseIn instanceof Player) {
+							scale = 0.7F * i;
+							matrixStackIn.scale(scale, scale * 0.6F, scale);
+							matrixStackIn.translate(0.0D, (double) (-1.2F + 0.6F * (float) i), 0.0D);
+						} else {
+							scale = 0.55F * i;
+							matrixStackIn.scale(scale, scale * 0.6F, scale);
+						}
+						break;
+
+					}
+					this.box.render(matrixStackIn, vertexconsumer, packedLightIn, OverlayTexture.NO_OVERLAY);
+					matrixStackIn.popPose();
+				}
 			}
 		}
 	}
