5454991abb19678035bd49c4b505fde4362b7e6d
Xephiro9999 <dreadbot8001@gmail.com>
2024-04-07 16:20:37 -0400

    Create WayfinderItem.java

---
+++ src/main/java/online/kingdomkeys/kingdomkeys/item/WayfinderItem.java
@@ -0,0 +1,63 @@
+package online.kingdomkeys.kingdomkeys.item;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import online.kingdomkeys.kingdomkeys.api.item.ItemCategory;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+public class WayfinderItem extends Item {
+    public WayfinderItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(entityIn instanceof Player player){
+            if (stack.getTag() != null) {
+                if (!stack.getTag().hasUUID("ownerUUID"))
+                    stack.setTag(setID(stack.getTag(), player));
+            } else {
+                stack.setTag(setID(new CompoundTag(), player));
+            }
+            super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+    }
+        }
+
+
+
+    public CompoundTag setID(CompoundTag nbt, Player player) {
+        nbt.putUUID("ownerUUID", player.getUUID());
+        return nbt;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        return super.use(world, player, hand);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+        @Override
+        public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        tooltip = ("Owner: " + Player);
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack pStack) {
+        return false;
+    }
+
+
+}
