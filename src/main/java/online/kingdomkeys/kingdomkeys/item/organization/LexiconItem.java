1e2abd67749a4fb0197151d3a6045e649b2ffaf9
Xephiro9999 <dreadbot8001@gmail.com>
2024-04-16 12:24:10 -0400

    Update LexiconItem.java

--- src/main/java/online/kingdomkeys/kingdomkeys/item/organization/LexiconItem.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/item/organization/LexiconItem.java
@@ -1,5 +1,13 @@
 package online.kingdomkeys.kingdomkeys.item.organization;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import online.kingdomkeys.kingdomkeys.capability.IPlayerCapabilities;
+import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.util.Utils;
 
 public class LexiconItem extends OrgSwordItem implements IOrgWeapon {
@@ -7,6 +15,16 @@ public class LexiconItem extends OrgSwordItem implements IOrgWeapon {
     @Override
     public Utils.OrgMember getMember() {
         return Utils.OrgMember.ZEXION;
+    }
+
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        IPlayerCapabilities playerData = ModCapabilities.getPlayer(player);
+        
+        player.playSound(SoundEvents.BOOK_PAGE_TURN, 1F, 1F);
+
+        return super.use(world, player, hand);
     }
 
 }
