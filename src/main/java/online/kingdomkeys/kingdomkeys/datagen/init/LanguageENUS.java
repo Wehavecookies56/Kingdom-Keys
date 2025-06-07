ff4a8cab5ce0f81d49ed981954afc215af279b06
Abel <abel54225@gmail.com>
2025-06-07 19:51:05 +0200

    Fixed Music Disc names and tooltips Updated spanish translation for the last 7 tracks.

--- src/main/java/online/kingdomkeys/kingdomkeys/datagen/init/LanguageENUS.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/datagen/init/LanguageENUS.java
@@ -1566,26 +1566,26 @@ public class LanguageENUS extends KKLanguageProvider {
         //Discs
         add("disc.duration.desc", "Duration");
         add("disc.durationunits.desc", "(mins:secs)");
-        addMusicDisc(ModItems.disc_Birth_by_Sleep_A_Link_to_the_Future, "Yoko Shimomura & Kaoru Wada - Birth by Sleep -A Link to the Future-");
-        addMusicDisc(ModItems.disc_Dream_Drop_Distance_The_Next_Awakening, "Yoko Shimomura & Kaoru Wada - Dream Drop Distance -The Next Awakening-");
-        addMusicDisc(ModItems.disc_Hikari_KINGDOM_Instrumental_Version, "Yoko Shimomura & Kaoru Wada - Hikari -KINGDOM Instrumental Version-");
-        addMusicDisc(ModItems.disc_L_Oscurita_Dell_Ignoto, "Yoko Shimomura - L'Oscurita Dell'Ignoto");
-        addMusicDisc(ModItems.disc_Musique_pour_la_tristesse_de_Xion, "Yoko Shimomura - Musique pour la tristesse de Xion");
-        addMusicDisc(ModItems.disc_No_More_Bugs_Bug_Version, "Yoko Shimomura & Hirosato Noda - No More Bugs!! -Bug Version-");
-        addMusicDisc(ModItems.disc_Organization_XIII, "Yoko Shimomura - Organization XIII");
-        addMusicDisc(ModItems.disc_Dearly_Beloved_UX, "Yoko Shimomura - Dearly Beloved -Union \u03c7 Credits Version-");
-        addMusicDisc(ModItems.disc_Passion_Instrumental, "Yoko Shimomura - Passion -Kingdom Orchestra Instrumental Version-");
-        addMusicDisc(ModItems.disc_Rage_Awakened, "Yoko Shimomura - Rage Awakened");
-        addMusicDisc(ModItems.disc_The_Other_Promise, "Yoko Shimomura - The Other Promise");
-        addMusicDisc(ModItems.disc_13th_Struggle_Luxord, "Yuko Komiyama - 13th Struggle -Luxord-");
-        addMusicDisc(ModItems.disc_13th_Dilemma_Saix, "Yuki Kishida - 13th Dilemma -Saix-");
-        addMusicDisc(ModItems.disc_13th_Reflection, "Yoko Shimomura - 13th Reflection");
-        addMusicDisc(ModItems.disc_Another_Side_Battle_Ver, "Yoko Shimomura - Another Side -Battle Ver-");
-        addMusicDisc(ModItems.disc_Cavern_Of_Remembrance_Days, "Yoko Shimomura - Cavern of Remembrance -Days Version-");
-        addMusicDisc(ModItems.disc_Forgotten_Challenge_Recoded, "Yoko Shimomura - Forgotten Challenge -Re:Coded Version-");
-        addMusicDisc(ModItems.disc_Anger_Unchained, "Takeharu Ishimoto - Anger Unchained");
-        addMusicDisc(ModItems.disc_Hunter_Of_The_Dark, "Yoko Shimomura - Hunter of the Dark");
-        addMusicDisc(ModItems.disc_Destati, "Yoko Shimomura - Destati");        
+        addMusicDisc(ModItems.disc_Birth_by_Sleep_A_Link_to_the_Future, "Birth by Sleep -A Link to the Future-");
+        addMusicDisc(ModItems.disc_Dream_Drop_Distance_The_Next_Awakening, "Dream Drop Distance -The Next Awakening-");
+        addMusicDisc(ModItems.disc_Hikari_KINGDOM_Instrumental_Version, "Hikari -KINGDOM Instrumental Version-");
+        addMusicDisc(ModItems.disc_L_Oscurita_Dell_Ignoto, "L'Oscurita Dell'Ignoto");
+        addMusicDisc(ModItems.disc_Musique_pour_la_tristesse_de_Xion, "Musique pour la tristesse de Xion");
+        addMusicDisc(ModItems.disc_No_More_Bugs_Bug_Version, "No More Bugs!! -Bug Version-");
+        addMusicDisc(ModItems.disc_Organization_XIII, "Organization XIII");
+        addMusicDisc(ModItems.disc_Dearly_Beloved_UX, "Dearly Beloved -Union \u03c7 Credits Version-");
+        addMusicDisc(ModItems.disc_Passion_Instrumental, "Passion -Kingdom Orchestra Instrumental Version-");
+        addMusicDisc(ModItems.disc_Rage_Awakened, "Rage Awakened");
+        addMusicDisc(ModItems.disc_The_Other_Promise, "The Other Promise");
+        addMusicDisc(ModItems.disc_13th_Struggle_Luxord, "13th Struggle -Luxord-");
+        addMusicDisc(ModItems.disc_13th_Dilemma_Saix, "13th Dilemma -Saix-");
+        addMusicDisc(ModItems.disc_13th_Reflection, "13th Reflection");
+        addMusicDisc(ModItems.disc_Another_Side_Battle_Ver, "Another Side -Battle Ver-");
+        addMusicDisc(ModItems.disc_Cavern_Of_Remembrance_Days, "Cavern of Remembrance -Days Version-");
+        addMusicDisc(ModItems.disc_Forgotten_Challenge_Recoded, "Forgotten Challenge -Re:Coded Version-");
+        addMusicDisc(ModItems.disc_Anger_Unchained, "Anger Unchained");
+        addMusicDisc(ModItems.disc_Hunter_Of_The_Dark, "Hunter of the Dark");
+        addMusicDisc(ModItems.disc_Destati, "Destati");
 
         //Command Menu Items
         addItem(ModItems.potion, "Potion");
--- src/main/java/online/kingdomkeys/kingdomkeys/item/KKRecordItem.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/item/KKRecordItem.java
@@ -1,20 +1,36 @@
 package online.kingdomkeys.kingdomkeys.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.JukeboxSong;
+import net.minecraft.world.item.*;
 import online.kingdomkeys.kingdomkeys.api.item.IItemCategory;
 import online.kingdomkeys.kingdomkeys.api.item.ItemCategory;
+import online.kingdomkeys.kingdomkeys.util.Utils;
+
+import java.util.List;
 
 public class KKRecordItem extends Item implements IItemCategory {
-
-
     public KKRecordItem(ResourceKey<JukeboxSong> song) {
-        super(new Properties().stacksTo(1).jukeboxPlayable(song));
+        super(new Properties().stacksTo(1).rarity(Rarity.RARE).jukeboxPlayable(song));
     }
 
     @Override
     public ItemCategory getCategory() {
         return ItemCategory.MISC;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("item.kingdomkeys."+Utils.getItemRegistryName(this).getPath()+".desc"));
+
+        JukeboxSong song = JukeboxSong.fromStack(context.registries(), stack).get().value();
+        if(song != null) {
+            int length = (int) song.lengthInSeconds();
+            int minutes = length / 60;
+            int seconds = length % 60;
+            tooltipComponents.add( Component.literal(Utils.translateToLocal("disc.duration.desc")+": %d:%02d".formatted(minutes,seconds)).withStyle(ChatFormatting.GRAY));
+        }
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
