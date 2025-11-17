3c4be4a4bcc8ff3dab4a6c5e8403b19034d9c838
Estelle <qwenit56@gmail.com>
2025-11-17 11:02:43 +0000

    cache pauldron baked models for efm and fix keyblade selection overlap fixes #116

--- src/main/java/online/kingdomkeys/kingdomkeys/integration/epicfight/PatchedShoulderLayerRenderer.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/integration/epicfight/PatchedShoulderLayerRenderer.java
@@ -17,12 +17,15 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import online.kingdomkeys.kingdomkeys.KingdomKeys;
 import online.kingdomkeys.kingdomkeys.capability.ModCapabilities;
 import online.kingdomkeys.kingdomkeys.client.render.ShoulderLayerRenderer;
 import online.kingdomkeys.kingdomkeys.item.ModItems;
 import online.kingdomkeys.kingdomkeys.item.PauldronItem;
+import yesman.epicfight.api.client.forgeevent.PrepareModelEvent;
 import yesman.epicfight.api.client.model.Mesh;
 import yesman.epicfight.api.client.model.Meshes;
 import yesman.epicfight.api.client.model.SkinnedMesh;
@@ -35,11 +38,21 @@ import yesman.epicfight.client.renderer.patched.layer.PatchedLayer;
 import yesman.epicfight.gameasset.Armatures;
 import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PatchedShoulderLayerRenderer<E extends LivingEntity, T extends LivingEntityPatch<E>, M extends HumanoidModel<E>> extends PatchedLayer<E, T, M, ShoulderLayerRenderer<E, M>> {
     public PatchedShoulderLayerRenderer() {
     }
 
     ResourceLocation texture;
+    public static final Map<Item, SkinnedMesh> epicfight_shoulderModels = new HashMap<>();
+
+    @SubscribeEvent
+    public static void clearModels(PrepareModelEvent meshBuildEvent) {
+        epicfight_shoulderModels.values().forEach(SkinnedMesh::destroy);
+        epicfight_shoulderModels.clear();
+    }
 
     @Override
     protected void renderLayer(T t, E e, ShoulderLayerRenderer<E, M> emRenderLayer, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, OpenMatrix4f[] openMatrix4fs, float bob, float v, float v1, float v2) {
@@ -53,7 +66,9 @@ public class PatchedShoulderLayerRenderer<E extends LivingEntity, T extends Livi
             model = models.get(armorName);
             if (model != null) {
                 model.leftArm.visible = true;
-                SkinnedMesh modelAnimated = HumanoidModelBaker.bakeArmor(player, armor, (ArmorItem) ModItems.terra_Helmet.get(), EquipmentSlot.CHEST, emRenderLayer.getParentModel(), model, emRenderLayer.getParentModel(), Meshes.BIPED.get());
+                if (!epicfight_shoulderModels.containsKey(armor.getItem())) {
+                    epicfight_shoulderModels.put(armor.getItem(), HumanoidModelBaker.bakeArmor(player, armor, (ArmorItem) ModItems.terra_Helmet.get(), EquipmentSlot.CHEST, emRenderLayer.getParentModel(), model, emRenderLayer.getParentModel(), Meshes.BIPED.get()));
+                }
                 texture = new ResourceLocation(KingdomKeys.MODID, "textures/models/armor/"+armorName+"_shoulder.png");
                 model.setAllVisible(true);
                 AbstractClientPlayer clientPlayer = (AbstractClientPlayer) player;
@@ -63,7 +78,7 @@ public class PatchedShoulderLayerRenderer<E extends LivingEntity, T extends Livi
                 VertexConsumer bufferBuilder = multiBufferSource.getBuffer(EpicFightRenderTypes.getTriangulated(EpicFightRenderTypes.armorCutoutNoCull(texture)));
                 if (steve)
                     poseStack.translate(-0.07, 0, 0);
-                modelAnimated.drawPosed(poseStack, bufferBuilder, Mesh.DrawingFunction.NEW_ENTITY, packedLight, 1, 1, 1, 1, OverlayTexture.NO_OVERLAY, Armatures.BIPED.get(), openMatrix4fs);
+                epicfight_shoulderModels.get(armor.getItem()).drawPosed(poseStack, bufferBuilder, Mesh.DrawingFunction.NEW_ENTITY, packedLight, 1, 1, 1, 1, OverlayTexture.NO_OVERLAY, Armatures.BIPED.get(), openMatrix4fs);
                 poseStack.popPose();
             }
         }
