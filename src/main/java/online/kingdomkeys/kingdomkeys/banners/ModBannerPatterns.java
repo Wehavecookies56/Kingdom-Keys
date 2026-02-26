73b89b6af60dae9d1ac33aa81b62a3705f691168
Abel <abel54225@gmail.com>
2026-02-26 17:33:09 +0100

    Added filled variants

--- src/main/java/online/kingdomkeys/kingdomkeys/banners/ModBannerPatterns.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/banners/ModBannerPatterns.java
@@ -12,7 +12,12 @@ public class ModBannerPatterns {
     public static final DeferredRegister<BannerPattern> PATTERNS = DeferredRegister.create(Registries.BANNER_PATTERN, KingdomKeys.MODID);
 
     public static final Supplier<BannerPattern>
-            HEARTLESS = PATTERNS.register("heartless", () -> new BannerPattern(ResourceLocation.fromNamespaceAndPath(KingdomKeys.MODID,"heartless"), "heartless")),
-            NOBODY = PATTERNS.register("nobody", () -> new BannerPattern(ResourceLocation.fromNamespaceAndPath(KingdomKeys.MODID,"nobody"), "nobody"));
+            HEARTLESS_OUTLINE = PATTERNS.register("heartless_outline", () -> createBanner("heartless_outline")),
+            NOBODY_OUTLINE = PATTERNS.register("nobody_outline", () -> createBanner("nobody_outline")),
+            HEARTLESS_FILLED = PATTERNS.register("heartless_filled", () -> createBanner("heartless_filled")),
+            NOBODY_FILLED = PATTERNS.register("nobody_filled", () -> createBanner("nobody_filled"));
 
+    public static BannerPattern createBanner(String name){
+        return new BannerPattern(ResourceLocation.fromNamespaceAndPath(KingdomKeys.MODID, name), name);
+    }
 }
