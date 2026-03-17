3aa10bcac2d4ccd54aa687bffb73ff8745f9545a
Abel <abel54225@gmail.com>
2026-03-17 19:55:26 +0100

    Submenu render event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,20 +1,6 @@
 **Added:**
-* Flowstep ability, allows airstepping to mobs at the cost of some Focus points (relative to the distance).
-  * Unlocked at level 33 for Warrior, 37 for Mystic and 40 for Guardian.
-* Sound when toggling the selected Reaction Command.
-* Interpolated values for MP Bar while on recharge.
-* Magic level field to Magic Cast Event.
-* Config option to prevent survival players from setting a global savepoint.
+* New Command SubMenu render event so addons can intercept it
 
 **Changed:**
-* Winner Stick is now able to be sold and no longer right clicked on a moogle.
-* KO effect goes from neutral to beneficial, hopefully fixing the Artifacts' Antidote vessel conflict.
-* Menu bars will now adapt their length based on the text they contain.
-* Button dot icon.
-* Font for the Command Menu header.
 
 **Fixed:**
-* Reaction command slot being covered below a submenu if actual submenu was targets.
-* Moogles not able to be renamed.
-* Lock on swap not working.
-* Aero and Stop models not rendering while in EFM.
--- src/main/java/online/kingdomkeys/kingdomkeys/client/gui/elements/CommandMenuSubMenu.java
+++ src/main/java/online/kingdomkeys/kingdomkeys/client/gui/elements/CommandMenuSubMenu.java
@@ -503,18 +503,22 @@ public class CommandMenuSubMenu {
             if (parent != null) {
                 updatePosition();
             }
+
             RenderSystem.enableBlend();
-            guiGraphics.pose().translate(0, 0, getZ());
-            guiGraphics.setColor(getColour().getRed() / 255F, getColour().getGreen() / 255F, getColour().getBlue() / 255F, 1);
-            if (useFixedHeader) {
-                guiGraphics.blit(getTexture(), getX(), getY(), 0, 70, 74, 15);
-            } else {
-                guiGraphics.blit(getTexture(), getX(), getY(), 0, 0, ModConfigs.cmHeaderEndLWidth, getHeight());
-                guiGraphics.blit(getTexture(), getX() + ModConfigs.cmHeaderEndLWidth, getY(), getWidth() - (ModConfigs.cmHeaderEndLWidth + ModConfigs.cmHeaderEndRWidth), getHeight(), ModConfigs.cmHeaderEndLWidth + 1, 0, 1, getHeight(), 256, 256);
-                guiGraphics.blit(getTexture(), getX() + getWidth() - ModConfigs.cmHeaderEndRWidth, getY(), ModConfigs.cmHeaderEndLWidth + 3, 0, ModConfigs.cmHeaderEndRWidth, getHeight());
-            }
-            if(ModConfigs.cmHeaderTextVisible) {
-                guiGraphics.drawCenteredString(Minecraft.getInstance().font, getTitle(), getX() + ((getWidth()-8)/2) + 1, getY() + 4, 0xFFFFFF);
+
+            if (!NeoForge.EVENT_BUS.post(new CommandMenuEvent.SubmenuRender(getId(), this, guiGraphics, screenWidth, screenHeight, partialTick)).isCanceled()) {
+                guiGraphics.pose().translate(0, 0, getZ());
+                guiGraphics.setColor(getColour().getRed() / 255F, getColour().getGreen() / 255F, getColour().getBlue() / 255F, 1);
+                if (useFixedHeader) {
+                    guiGraphics.blit(getTexture(), getX(), getY(), 0, 70, 74, 15);
+                } else {
+                    guiGraphics.blit(getTexture(), getX(), getY(), 0, 0, ModConfigs.cmHeaderEndLWidth, getHeight());
+                    guiGraphics.blit(getTexture(), getX() + ModConfigs.cmHeaderEndLWidth, getY(), getWidth() - (ModConfigs.cmHeaderEndLWidth + ModConfigs.cmHeaderEndRWidth), getHeight(), ModConfigs.cmHeaderEndLWidth + 1, 0, 1, getHeight(), 256, 256);
+                    guiGraphics.blit(getTexture(), getX() + getWidth() - ModConfigs.cmHeaderEndRWidth, getY(), ModConfigs.cmHeaderEndLWidth + 3, 0, ModConfigs.cmHeaderEndRWidth, getHeight());
+                }
+                if (ModConfigs.cmHeaderTextVisible) {
+                    guiGraphics.drawCenteredString(Minecraft.getInstance().font, getTitle(), getX() + ((getWidth() - 8) / 2) + 1, getY() + 4, 0xFFFFFF);
+                }
             }
             renderChildren(guiGraphics, screenWidth, screenHeight, partialTick);
 
